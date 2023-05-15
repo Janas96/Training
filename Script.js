d750e46eb93e7508174dfcef121da4abcad3931e
Janas96 <133674824+Janas96@users.noreply.github.com>
2023-05-15 18:02:51 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,16 +1,22 @@
-var a = "Siema"
-var b = "Ziom bbb bb hhh"
-function WardMarger (firstWord, secondWord)
-{
-    var sentence = firstWord + " " + secondWord;
-    console.log(sentence)
-    if(firstWord === "Siema")
-        console.log(firstWord + " " + "Siema")
-    else
-        console.log(secondWord + " " + "Ziom")
+// var a = "Siema"
+// var b = "Ziom bbb bb hhh"
+// function WardMarger (firstWord, secondWord)
+// {
+//     var sentence = firstWord + " " + secondWord;
+//     console.log(sentence)
+//     if(firstWord === "Siema")
+//         console.log(firstWord + " " + "Siema")
+//     else
+//         console.log(secondWord + " " + "Ziom")
 
-    const myArray = sentence.split(" ");
-    console.log(myArray.length);
-}   
+//     const myArray = sentence.split(" ");
+//     console.log(myArray.length);
+// }   
 
-WardMarger(a, b)
+// WardMarger(a, b)
+const sentence = "Mary on a cross Not just another bloody Mary Mary on a, Mary on a... Klesáš úplně jako Svatá Marie Marie na, Marie na kříži Ne jenom další krvavá Marie Marie na, Marie na kříži... You go down just like"
+console.log(sentence.length)
+console.log(sentence.replace(/\s+/g, '').length)
+let test = sentence.match(/[aA]/g);
+console.log(test)
+console.log(test.length)
